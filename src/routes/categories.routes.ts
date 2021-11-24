254f9bb1ea3269f8f3a7680cc78091566fa03f29
Racchel <racchelvelasco@gmail.com>
2021-11-24 12:53:43 -0300

    Validating category registration

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { request, Router } from "express";
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
@@ -7,9 +7,23 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;    
     
+    const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+    if(categoryAlreadyExists) {
+        return response.status(400).json({
+            error: "Category already exists"
+        })
+    }
+
     categoriesRepository.create({name, description});
 
     return response.status(201).send();
+})
+
+categoriesRoutes.get("/", (request, response) => {
+    const all = categoriesRepository.list();
+
+    return response.json(all);
 })
 
 export { categoriesRoutes };
